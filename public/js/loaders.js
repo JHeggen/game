cd8beb92b2dd8bede4d00588db4849941690b993
JHeggen <jonathan.heggen@gmail.com>
2019-10-06 16:24:50 -0700

    refactoring: loaders

--- public/js/loaders.js
+++ public/js/loaders.js
@@ -1,40 +1,80 @@
 import Level from './Level.js';
+import SpriteSheet from './SpriteSheet.js'
 import {createBackgroundLayer, createSpriteLayer} from './layers.js';
-import {loadBackgroundSprites} from './sprites.js';
 
-export function loadTiles(location) {
+export function loadImage(url) {
     return new Promise(resolve => {
         const image = new Image();
         image.addEventListener('load', () => {
             resolve(image);
         });
-        image.src = location;
+        image.src = url;
     });
 }
 
+function loadJSON(url) {
+    return fetch(url)
+    .then(r => r.json());
+}
+
 function createTiles(level, backgrounds) {
+    function applyRange(background, xStart, xLen, yStart, yLen) {
+        const xEnd = xStart + xLen;
+        const yEnd = yStart + yLen;
+        for (let x = xStart; x < xEnd; ++x) {
+            for (let y = yStart; y < yEnd; ++y) {
+                level.tiles.set(x, y, {
+                    name: background.tile,
+                });
+            }
+        }
+    }
+
     backgrounds.forEach(background => {
-        background.ranges.forEach(([xStart, xLen, yStart, yLen]) => {
-            const xEnd = xStart + xLen;
-            const yEnd = yStart + yLen;
-            for (let x = xStart; x < xEnd; ++x) {
-                for (let y = yStart; y < yEnd; ++y) {
-                    level.tiles.set(x, y, {
-                        name: background.tile,
-                    });
-                }
+        background.ranges.forEach(range => {
+            if (range.length === 4) {
+                const [xStart, xLen, yStart, yLen] = range;
+                applyRange(background, xStart, xLen, yStart, yLen);
+            } else if (range.length === 3) {
+                const [xStart, xLen, yStart] = range;
+                applyRange(background, xStart, xLen, yStart, 1);
+            } else if (range.length === 2) {
+                const [xStart, yStart] = range;
+                applyRange(background, xStart, 1, yStart, 1);
             }
         });
     });
 }
 
-export function loadLevel(name) {
-    return Promise.all([
-        fetch(`/levels/${name}.json`)
-        .then(r => r.json()),
-        loadBackgroundSprites(),
-    ])
+function loadSpriteSheet(name) {
+    return loadJSON(`/sprites/${name}.json`)
+    .then(sheetSpec => Promise.all([
+        sheetSpec,
+        loadImage(sheetSpec.imageURL),
+    ]))
+    .then(([sheetSpec, image]) => {
+        const sprites = new SpriteSheet(
+            image,
+            sheetSpec.tileW,
+            sheetSpec.tileH);
 
+        sheetSpec.tiles.forEach(tileSpec => {
+            sprites.defineTile(
+                tileSpec.name,
+                tileSpec.index[0],
+                tileSpec.index[1]);
+        });
+
+        return sprites;
+    });
+}
+ 
+export function loadLevel(name) {
+    return loadJSON(`/levels/${name}.json`)
+    .then(levelSpec => Promise.all([
+        levelSpec,
+        loadSpriteSheet(levelSpec.spriteSheet),
+    ]))
     .then(([levelSpec, backgroundSprites]) => {
         const level = new Level();
 
@@ -42,7 +82,7 @@ export function loadLevel(name) {
 
         const backgroundLayer = createBackgroundLayer(level, backgroundSprites);
         level.comp.layers.push(backgroundLayer);
-        
+
         const spriteLayer = createSpriteLayer(level.entities);
         level.comp.layers.push(spriteLayer);
 
--- public/js/sprites.js
+++ public/js/sprites.js
@@ -1,21 +1,11 @@
 import SpriteSheet from './SpriteSheet.js'
-import {loadTiles} from './loaders.js'
+import {loadImage} from './loaders.js'
 
 export function loadPlayerSprite() {
-    return loadTiles('/assets/tileset.png')
+    return loadImage('/assets/tileset.png')
     .then(image => {
         const sprites = new SpriteSheet(image, 32, 32);
         sprites.defineTile('idle', 6, 8);
-        return sprites;
-    });    
-}
-
-export function loadBackgroundSprites() {
-    return loadTiles('/assets/tileset.png')
-    .then(image => {
-        const sprites = new SpriteSheet(image, 32, 32);
-        sprites.defineTile('ground', 14, 2);
-        sprites.defineTile('floor', 15, 0);
         return sprites;
     });    
 }
