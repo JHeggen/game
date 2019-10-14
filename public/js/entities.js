d881c6a5944e0f7cfbd750d593b704e043eed538
JHeggen <jonathan.heggen@gmail.com>
2019-10-14 14:58:02 -0700

    left and right anims

--- public/js/entities.js
+++ public/js/entities.js
@@ -2,6 +2,7 @@ import Entity from './Entity.js'
 import Jump from './traits/Jump.js'
 import Go from './traits/Go.js'
 import {loadSpriteSheet} from './loaders.js'
+import {createAnim} from './anim.js';
 
 export function createPlayer() {
     return loadSpriteSheet('player')
@@ -12,20 +13,18 @@ export function createPlayer() {
         player.addTrait(new Go());
         player.addTrait(new Jump());
 
-        const frames = ['move-r1', 'move-r2', 'move-r3'];
+        const runAnim = createAnim(['move-r1', 'move-r2', 'move-r3'], 10);
 
         function routeFrame(player) {
             if (player.go.dir !== 0 ) {
-                const frameIndex = Math.floor(player.go.distance / 10) % frames.length;
-                const frameName = frames[frameIndex];
-                return frameName;
+                return runAnim(player.go.distance);
             }
 
             return 'idle';
         }
     
         player.draw = function drawPlayer(context) {
-            sprite.draw(routeFrame(this), context, 0, 0);
+            sprite.draw(routeFrame(this), context, 0, 0, player.go.dir < 0);
         }
     
         return player;
